b680646600be0e4d8ea8a76fea65532e75febcc9
TunarshBee <rafiubello4real@gmail.com>
2023-06-11 21:50:19 +0100

    done with tests for currently available urls

--- baseApp/tests/test_urls.py
+++ baseApp/tests/test_urls.py
@@ -1,7 +1,62 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+from baseApp.views import (activity, createRoom, deleteMsg, deleteRoom, editMsg, editUser, home,
+    loginUser, logoutUser, registerUser, room, topics, updateRoom, userProfile)
 
 
-class UrlTests(SimpleTestCase):
-    def home_url_resolves(self):
-        assert 1 == 2
+class TestUrls(SimpleTestCase):
+    def test_home_url_resolves(self):
+        home_url = reverse("home")
+        self.assertEquals(resolve(home_url).func, home)
+
+    def test_login_url_resolves(self):
+        login_url = reverse("login")
+        self.assertEquals(resolve(login_url).func, loginUser)
+
+    def test_register_url_resolves(self):
+        register_url = reverse("register")
+        self.assertEquals(resolve(register_url).func, registerUser)
+
+    def test_logout_url_resolves(self):
+        logout_url = reverse("logout")
+        self.assertEquals(resolve(logout_url).func, logoutUser)
+
+    def test_create_room_url_resolves(self):
+        create_room_url = reverse("createRoom")
+        self.assertEquals(resolve(create_room_url).func, createRoom)
+
+    def test_edit_user_url_resolves(self):
+        edit_user_url = reverse("edit_user")
+        self.assertEquals(resolve(edit_user_url).func, editUser)
+
+    def test_activity_url_resolves(self):
+        activity_url = reverse('activity')
+        self.assertEquals(resolve(activity_url).func, activity)
+        
+    def test_topics_url_resolves(self):
+        topics_url = reverse('topics')
+        self.assertEquals(resolve(topics_url).func, topics)
+
+    def test_room_url_resolves(self):
+        room_url = reverse("room", args=[1])
+        self.assertEquals(resolve(room_url).func, room)
+
+    def test_profile_url_resolves(self):
+        profile_url = reverse("profile", args=[1])
+        self.assertEquals(resolve(profile_url).func, userProfile)
+
+    def test_update_room_url_resolves(self):
+        updateRoom_url = reverse("updateRoom", args=[1])
+        self.assertEquals(resolve(updateRoom_url).func, updateRoom)
+
+    def test_delete_room_url_resolves(self):
+        deleteRoom_url = reverse("deleteRoom", args=[1])
+        self.assertEquals(resolve(deleteRoom_url).func, deleteRoom)
+
+    def test_delete_Msg_url_resolves(self):
+        deleteMsg_url = reverse("deleteMsg", args=[1])
+        self.assertEquals(resolve(deleteMsg_url).func, deleteMsg)
+
+    def test_edit_Msg_url_resolves(self):
+        editMsg_url = reverse("editMsg", args=[1])
+        self.assertEquals(resolve(editMsg_url).func, editMsg)
--- baseApp/urls.py
+++ baseApp/urls.py
@@ -2,25 +2,20 @@ from django.urls import path
 from . import views
 
 
-urlpatterns =[
-    path('register', views.registerUser, name='register'),
-    path('login', views.loginUser, name="login"),
-    path('logout', views.logoutUser, name="logout"),
-
-    path('', views.home, name="home"),
-    path('room/<int:idParam>/', views.room, name="room"),
-    path('profile/<int:id>', views.userProfile, name="profile"),
-    path('create_room', views.createRoom, name="createRoom" ),
-    path('update_room/<int:id>', views.updateRoom, name="updateRoom" ),
-    path('delete_room/<int:id>', views.deleteRoom, name="deleteRoom" ),
-    path('deleteMsg/<int:id>', views.deleteMsg, name="deleteMsg" ),
-
-    path('editMsg/<int:id>', views.editMsg, name="editMsg" ),
-    path('edit_user', views.editUser, name='edit_user'),
-
+urlpatterns = [
+    path("register", views.registerUser, name="register"),
+    path("login", views.loginUser, name="login"),
+    path("logout", views.logoutUser, name="logout"),
+    path("", views.home, name="home"),
+    path("room/<int:idParam>/", views.room, name="room"),
+    path("profile/<int:id>", views.userProfile, name="profile"),
+    path("create_room", views.createRoom, name="createRoom"),
+    path("update_room/<int:id>", views.updateRoom, name="updateRoom"),
+    path("delete_room/<int:id>", views.deleteRoom, name="deleteRoom"),
+    path("deleteMsg/<int:id>", views.deleteMsg, name="deleteMsg"),
+    path("editMsg/<int:id>", views.editMsg, name="editMsg"),
+    path("edit_user", views.editUser, name="edit_user"),
     # path('likeMsg/<str:id>', views.like, name='likeMsg'),
-    
-
-    path('activity', views.activity, name='activity'),
-    path('topics', views.topics, name='topics'),
+    path("activity", views.activity, name="activity"),
+    path("topics", views.topics, name="topics"),
 ]
